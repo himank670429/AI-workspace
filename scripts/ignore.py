6ec833cd2572875bd6116ebfe204a34eb4c5a750
Himank singh <singhhimank241@gmail.com>
2024-07-10 15:55:27 +0530

    changed workignore file format, no longer a binary file, now user can also directly modify the file and still use command line as per their prefrence

--- scripts/ignore.py
+++ scripts/ignore.py
@@ -1,24 +1,36 @@
 import pickle
 from os import path, getcwd
+
 class Ignore:
     __file_path = '.workspace\\.workignore'
     @staticmethod
     def getAll():
         try:
-            with open(path.join(getcwd(), Ignore.__file_path), 'rb+') as ignore_file:
-                while True:
-                    try:
-                        print(pickle.load(ignore_file))
-                    except EOFError:
-                        break
+            with open(path.join(getcwd(), Ignore.__file_path), 'r') as ignore_file:
+                file_content = ignore_file.readlines()
+                print("".join(file_content))
         except FileNotFoundError as e:
             print("FileNotFound: workspace not initialized :(")
             
     @staticmethod
     def add(arguments):
         try:
-            with open(path.join(getcwd(), Ignore.__file_path), 'ab') as ignore_file:
-                [pickle.dump(f'{i}', ignore_file)for i in arguments]
+            file_content = []
+            file_path = path.join(getcwd(), Ignore.__file_path)
+            with open(file_path, 'r') as ignore_file:
+                file_content = ignore_file.readlines()
+
+            # add new items
+            file_content = [i.strip() for i in file_content]
+            for i in arguments:
+                if i in file_content:
+                    continue
+                file_content.append(i)
+
+            # add the to file
+            with open(file_path, 'w') as ignore_file:
+                ignore_file.writelines([f'{i}\n' for i in file_content])
+
         except FileNotFoundError as e:
             print("FileNotFound: workspace not initialized :(")    
     
@@ -37,31 +49,24 @@ class Ignore:
 
         else:
             print("could not find .gitignore file :(")
-            
-
-    
+        
     @staticmethod
     def remove(arguments=None, all_flag=False):
         try:
             full_path = path.join(getcwd(), Ignore.__file_path)
             # read the content
             if (all_flag):
-                with open(full_path, 'wb') as ignore_file:
+                with open(full_path, 'w') as ignore_file:
                     return
             content = []
-            with open(full_path, 'rb') as ignore_file:
-                while True:
-                    try:
-                        content.append(pickle.load(ignore_file))
-                    except EOFError:
-                        break
+            with open(full_path, 'r') as ignore_file:
+                content = ignore_file.readlines()
+
+            content = [i.strip() for i in content]
             
             result_content = list(set(content) - set(arguments))
-            with open(full_path, 'wb') as ignore_file:
-                [pickle.dump(f'{i}', ignore_file)for i in result_content]
+            with open(full_path, 'w') as ignore_file:
+                ignore_file.writelines([f'{i}\n' for i in result_content])
 
         except FileNotFoundError as e:
-            print("FileNotFound: workspace not initialized :(")
-
-        
-            
+            print("FileNotFound: workspace not initialized :(")       
